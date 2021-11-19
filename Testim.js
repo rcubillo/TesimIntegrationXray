0295a6c5a6de9097403fc0f77912256e42abd674
Ronald Cubillo <ronaldcubillo92@gmail.com>
2021-11-19 09:09:26 -0600

    Delete Testim.js

--- Testim.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config()
-
-
-testim--token process.env.TOKEN--project process.env.PROJECT--grid process.env.GRID
-
-testim--token "3H6KvG710VRtis8e0j4nbH0Lc5MJIZzhCYsSjm2DOEENb7uNYk" --project "rMDuwZcHfy1l6jUu7cXo"  --grid "Testim-Grid" - l "Sanity Check" - r ~/report.xml
-
-testim--token "3H6KvG710VRtis8e0j4nbH0Lc5MJIZzhCYsSjm2DOEENb7uNYk" --project "rMDuwZcHfy1l6jUu7cXo"  --grid "Testim-Grid" - r ~/Documents/Visual\ Studio\ Code\ Folders / Fortress / TestimAutomationXray / junit.xml--testId "4So4099xDBdk8Kpz"
